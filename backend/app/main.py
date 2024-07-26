d56f1148ee3fa05b2754f5145e4fb55f4aced795
snehamalbhage <snehamalbhage4@gmail.com>
2024-07-26 17:50:38 +0530

    new origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-      allow_origins=["*"], 
+      allow_origins=["https://predixion-ai-assi.vercel.app/"], 
    
     allow_credentials=True,
     allow_methods=["*"],
